476d9c909a1d3970ef942bc739cbc2208a38a2e0
Emerson Tosin <emerson.tosin@gmail.com>
2023-11-03 14:32:34 +0000

    first commit

---
+++ exemplo.js
@@ -0,0 +1,37 @@
+/*let idade = "41";
+let idadeConv = parseInt(idade);
+let msg = `Eu tenho ${idadeConv} anos`;
+
+if (typeof idadeConv  == "number" || 1 == 2 ){
+    console.log(msg);
+} */
+
+/*function Pessoa () {
+    this.nome = "Emerson";
+    this.apelido = "Tosin";
+    this.idade   = 41;
+}
+
+let newPessoa = new Pessoa();
+
+console.log(newPessoa.nome); */
+
+let varIdade = 33.
+
+/*let varTexto = (varIdade >= 18) ? "Maior de idade" : "Menor de idade";
+console.log(varTexto); */
+
+const arrowFunction = (n1, n2) => { 
+
+   if(n1 == undefined){
+    n1 = 0
+   }
+
+   if(n2 == undefined){
+    n2 = 0
+   }
+    
+   return n1 + n2;
+}
+
+console.log( arrowFunction(2) );
